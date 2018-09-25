69344ce0b7f4a07e868fb9f24f45ad448fc28440
Dexter2772 <31225973+Dexter2772@users.noreply.github.com>
2018-09-25 17:32:34 -0400

    Add d3 code to parse data

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <style>
 
-rect {
+.bar {
   fill: steelblue;
 }
 
-text {
-  fill: white;
-  font: 10px sans-serif;
-  text-anchor: end;
+.bar:hover {
+  fill: brown;
 }
 
-
+.axis--x path {
+  display: none;
+}
+svg {
+  padding-left: 50px;
+}
 </style>
 <?php
 function loadCsv($path) {
@@ -23,129 +26,79 @@ function loadCsv($path) {
     array_shift($csv); # remove column header
     return $csv;
 }
-function calculateAverages($data) {
-  $averagedData = [];
-  forEach($data as $dataPoint){
-    $date = DateTime::createFromFormat("Y-m-d", $dataPoint['DATE']);
 
-    $year = $date->format("Y");
-    $month = $date->format("m");
+$tempData = loadCsv('http://localhost/dataAnalysisLab/HawaiiStats.csv');
 
-    if(!$averagedData[$year]){
-      $averagedData[$year] = [];
-    }
-    if(!$averagedData[$year][$month]){
-       $averagedData[intval($year)][intval($month)] = [
-         'tempTotal'=>intval($dataPoint['TMAX']),
-         'prcpTotal'=> intval($dataPoint['PRCP']),
-         'tempAvg'=>intval($dataPoint['TMAX']),
-         'prcpAvg'=> intval($dataPoint['PRCP']),
-         'numDays' => 1];
-    } else {
-       $existingMonthData = $averagedData[$year][$month];
-       $averagedData[$year][$month]['numDays']++;
+$population = array();
 
-       $averagedData[$year][$month]['tempTotal'] = $averagedData[$year][$month]['tempTotal'] + intval($dataPoint['tmax']);
-       $averagedData[$year][$month]['prcpTotal'] = $averagedData[$year][$month]['prcpTotal'] + intval($dataPoint['PRCP']);
-       $averagedData[$year][$month]['tempAvg'] = $averagedData[$year][$month]['tempTotal'] / $averagedData[$year][$month]['numDays'];
-       $averagedData[$year][$month]['prcpAvg'] = $averagedData[$year][$month]['prcpTotal'] / $averagedData[$year][$month]['numDays'];
-
-    }
-  }
-  return $averagedData;
-}
-$tempData = loadCsv('http://localhost/dataAnalysis/keywest-temperature.csv');
-// $tempData = loadCsv('http://localhost/dataAnalysis/miami-temperature.csv');
-$precipData = loadCsv('http://localhost/dataAnalysis/keywest-precipitation.csv');
-$mergedData = [];
-$i = 0;
-foreach($tempData as $temp) {
-  $mergedData[] = array_merge($temp, $precipData[$i]);
-  $i++;
+foreach($tempData as $data){
+  array_push($population, $data["Population"]);
 }
 
-$averageData = calculateAverages($mergedData);
-$tempData = [];
-// $tempData = "[";
-foreach($averageData as $data){
-  foreach($data as $month){
-    $tempData[] = $month['tempAvg'];
-    // $tempAvg = $month['tempAvg'];
-    // if($tempData == "[") {
-    //   $tempData = $tempData . "$tempAvg";
-    // } else {
-    //   $tempData = $tempData . ", $tempAvg";
-    // }
-  }
-}
-// echo("<input id='temp' type='hidden' value='".implode(", ", $tempData)."' />");
-
-// $tempData = $tempData . "]";
-// var_dump($tempData);
-// $tempData = '[80, 100, 56, 120, 180, 30, 40, 120, 160]';
-// $jsonTempData = json_encode($tempData);
 ?>
-<h1>Miami</h1>
+<h1>Hawaii Counties By Population (2018)</h1>
 <html>
   <head>
     <link rel="stylesheet" href="index.css">
   </head>
   <body>
-    <svg></svg>
+    <svg height="500" width="960"></svg>
     <script src="https://d3js.org/d3.v4.min.js"></script>
     <script>
-    var svgWidth = 5000;
-    var svgHeight = 300;
-    var svg = d3.select('svg')
-        .attr("width", svgWidth)
-        .attr("height", svgHeight)
-        .attr("class", "bar-chart");
 
-        var dataset = <?php echo("[" . implode(", ", $tempData) . "]"); ?>;
-        // var dataset = document.getElementById('temp').value;
-        console.log(dataset);
-        var barPadding = 5;
-        var barWidth = (svgWidth / dataset.length);
-        var barChart = svg.selectAll("rect")
-            .data(dataset)
-            .enter()
-            .append("rect")
-            .attr("y", function(d) {
-                return svgHeight - d
-            })
-            .attr("height", function(d) {
-                return d;
-            })
-            .attr("width", barWidth - barPadding)
-            .attr("transform", function (d, i) {
-                 var translate = [barWidth * i, 0];
-                 return "translate("+ translate +")";
-            });
-            d3.select("svg").transition()
-        .style("background-color", "black");
+    var svg = d3.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 40},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom;
+
+var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    y = d3.scaleLinear().rangeRound([height, 0]);
+
+var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    d3.csv("HawaiiStats.csv", function(d) {
+      d.Population = +d.Population;
+      console.log(d);
+      return d;
+    }, function(error, data) {
+      if (error) throw error;
+
+// d3.tsv("data.tsv", function(d) {
+//   d.frequency = +d.frequency;
+//   return d;
+// }, function(error, data) {
+//   if (error) throw error;
+
+  x.domain(data.map(function(d) { return d.County; }));
+  y.domain([0, d3.max(data, function(d) { return d.Population; })]);
+
+  g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+  g.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y).ticks(10))
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Population");
+
+  g.selectAll(".bar")
+    .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.County); })
+      .attr("y", function(d) { return y(d.Population); })
+      .attr("width", x.bandwidth())
+      .attr("height", function(d) { return height - y(d.Population); });
+});
+
       </script>
   </body>
 </html>
 
-<table class="table">
-  <tr>
-    <th>Year</th>
-    <th>Month</th>
-    <th>Temp</th>
-    <th>Precip</th>
-  </tr>
-<?php
-// foreach($averageData as $year => $data) {
-//   for($i = 01; $i <= 12; $i++){
-//     $temp = $data[$i]['tempAvg'];
-//     $precip = $data[$i]['prcpAvg'];
-//     echo("<tr>");
-//     echo("<td>$year</td>");
-//     echo("<td>$i</td>");
-//     echo("<td>$temp</td>");
-//     echo("<td>$precip</td>");
-//     echo("</tr>");
-//   }
-// }
-?>
 </table>
